ea52778bee6fcb8cf925c1f7596a18c8158a067a
syafiqparadisam <syafiqparadisam@gmail.com>
2024-08-27 14:08:50 +0700

    login, and get product by user

--- backend/app/Core/DB.php
+++ backend/app/Core/DB.php
@@ -5,7 +5,7 @@ use PDO;
 class DB {
 	public $db;
 	public function __construct() {
-		$this->db = new PDO("mysql:host=localhost;dbname=madura", "root");
+		$this->db = new PDO("mysql:host=localhost;dbname=dbtokomadura", "root");
 	}
 
 	public function getConn() {
--- backend/app/Models/UserModel.php
+++ backend/app/Models/UserModel.php
@@ -7,7 +7,7 @@ use PDO;
 class UserModel extends DB {
 	public function insertUser($user, $email, $password) {
 		try {
-		$query = $this->getConn()->prepare("INSERT INTO master_user (Username, Email, password) values (?,?,?)");
+		$query = $this->getConn()->prepare("INSERT INTO master_user (Username, Email, Password) values (?,?,?)");
 		$query->execute([$user, $email, $password]);
 
 		$result = $query->fetch(PDO::FETCH_ASSOC);
@@ -15,5 +15,13 @@ class UserModel extends DB {
 		} catch (\PDOException $e) {
 			echo $e->getMessage();
 		}
+	}
+
+	public function checkToken($token) {
+		$query = $this->getConn()->prepare("SELECT token from master_user where token = ?");
+		$query->execute([$token]);
+
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		return $result;
 	}
 }
