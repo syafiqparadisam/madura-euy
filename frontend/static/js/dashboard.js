8ca83c7e3d4755c281d919a9e131c5fabc4288aa
africanCLAM <rafir0532@gmail.com>
2024-08-27 11:29:08 +0700

    push prep

---
+++ frontend/static/js/dashboard.js
@@ -0,0 +1,33 @@
+export async function showBigProduct() {
+        $("#dashboard").append(`
+            <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">kode</th>
+                <th scope="col">Nama</th>
+                <th scope="col">kategori</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+      `);
+}
---
+++ frontend/static/js/views/Dashboard.js
@@ -0,0 +1,21 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor(params) {
+        super(params);
+    }
+
+    async getHtml(id) {
+        return `
+            <div class="text-start mt-3 ml-3" id="backButton">
+              <button class="text-4xl">
+              <i class="bi bi-arrow-left-circle-fill text-dark"></i>
+              </button>
+           </div>
+           <div class="max-w-screen-xl mx-auto mt-10 p-4 text-white flex flex-col min-h-screen" id="dashboard" >
+           </div>
+        `;
+    }
+}
+
+
